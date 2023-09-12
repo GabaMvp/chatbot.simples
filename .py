e87c16d6450f82eed44f1589b90e89d689fc0991
GabaMvp <142193066+GabaMvp@users.noreply.github.com>
2023-09-12 20:07:05 -0300

    Update .py

--- .py
+++ .py
@@ -1,5 +1,8 @@
 import nltk
 from nltk.chat.util import Chat, reflections
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QHBoxLayout, QTextEdit, QPushButton, QLabel, QLineEdit
+from PyQt5.QtGui import QTextCursor 
 
 # Pares de padrão e respostas para o chatbot
 pares = [
@@ -9,22 +12,48 @@ pares = [
     ["Qual é o seu nome?", ["Me chamam de gabaJr.", "Pode me chamar de chatbot."]],
     ["Qual é o sentido da vida?", ["Eu também gostaria de saber"]],
     ["Tchau|Até logo", ["Até mais!", "Tchau!"]]
-    ]
+]
 
 # Criando o objeto Chat com os pares de padrão e resposta
 chatbot = Chat(pares, reflections)
 
-#Função para integragir com o chatbot
-def interagir():
-    print("Chatbot: Olá! Como posso ajudar?")
-    while True:
-        entrada = input("Você:")
-        if entrada.lower() == "sair":
-            print("Chatbot: Até mais!")
-            break
-        resposta = chatbot.respond(entrada)
-        print("Chatbot:", resposta)
+class ChatbotApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-# Iniciando a interação com o chatbot
-interagir()
+    def initUI(self):
+        self.setWindowTitle('Chatbot')
+        self.setGeometry(100, 100, 800, 600)
 
+        self.layout = QVBoxLayout()
+
+        self.chat_history = QTextEdit(self)
+        self.chat_history.setReadOnly(True)
+        self.layout.addWidget(self.chat_history)
+
+        self.user_input = QLineEdit(self)
+        self.layout.addWidget(self.user_input)
+
+        self.send_button = QPushButton('Enviar', self)
+        self.send_button.clicked.connect(self.send_message)
+        self.layout.addWidget(self.send_button)
+
+        self.setLayout(self.layout)
+
+    def send_message(self):
+        user_text = self.user_input.text()
+        self.user_input.clear()
+
+        response = chatbot.respond(user_text)
+        self.display_message(f"Você: {user_text}\nChatbot: {response}\n")
+
+    def display_message(self, message):
+        self.chat_history.insertPlainText(message)
+        self.chat_history.moveCursor(QTextCursor.End)  # Corrigindo a linha aqui
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = ChatbotApp()
+    ex.show()
+    sys.exit(app.exec_())
